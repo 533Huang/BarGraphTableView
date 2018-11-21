e94b0095193336ad789f2bc228eaf105321e640e
mac <mac@macdemacbook-pro-2.local>
2018-11-21 15:04:31 +0800

    Initial Commit

---
+++ BarGraphTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarGraphTableView
+//
+//  Created by mac on 2018/11/21.
+//  Copyright © 2018年 Yongya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BarGraphTableViewTests/BarGraphTableViewTests.m
@@ -0,0 +1,37 @@
+//
+//  BarGraphTableViewTests.m
+//  BarGraphTableViewTests
+//
+//  Created by mac on 2018/11/21.
+//  Copyright © 2018年 Yongya. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BarGraphTableViewTests : XCTestCase
+
+@end
+
+@implementation BarGraphTableViewTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
